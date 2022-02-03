adb1269949e1d39f52c2d64a1e8498c986f22f09
Ivan Gunko <gamercombat57@gmail.com>
2022-02-03 19:11:26 +0200

    Releas

--- project/Form1.Designer.cs
+++ project/Form1.Designer.cs
@@ -31,15 +31,16 @@
             this.showLoginButton = new System.Windows.Forms.Button();
             this.showRegisterButton = new System.Windows.Forms.Button();
             this.loginPanel = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.registerPanel = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -74,26 +75,28 @@
             // 
             // loginPanel
             // 
-            this.loginPanel.Controls.Add(this.button3);
+            this.loginPanel.BackColor = System.Drawing.Color.DarkOrange;
+            this.loginPanel.Controls.Add(this.LoginButton);
             this.loginPanel.Controls.Add(this.label2);
             this.loginPanel.Controls.Add(this.label1);
             this.loginPanel.Controls.Add(this.textBox2);
             this.loginPanel.Controls.Add(this.textBox1);
             this.loginPanel.Location = new System.Drawing.Point(13, 56);
             this.loginPanel.Name = "loginPanel";
-            this.loginPanel.Size = new System.Drawing.Size(372, 351);
+            this.loginPanel.Size = new System.Drawing.Size(381, 351);
             this.loginPanel.TabIndex = 2;
             // 
-            // button3
+            // LoginButton
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(14, 159);
-            this.button3.Margin = new System.Windows.Forms.Padding(0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 40);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Login";
-            this.button3.UseVisualStyleBackColor = true;
+            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginButton.Location = new System.Drawing.Point(14, 159);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(0);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(95, 40);
+            this.LoginButton.TabIndex = 3;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
             // label2
             // 
@@ -134,17 +137,30 @@
             // 
             // registerPanel
             // 
+            this.registerPanel.BackColor = System.Drawing.Color.GreenYellow;
+            this.registerPanel.Controls.Add(this.textBox4);
+            this.registerPanel.Controls.Add(this.label6);
             this.registerPanel.Controls.Add(this.label5);
             this.registerPanel.Controls.Add(this.textBox5);
-            this.registerPanel.Controls.Add(this.button4);
+            this.registerPanel.Controls.Add(this.RegisterButton);
             this.registerPanel.Controls.Add(this.label3);
             this.registerPanel.Controls.Add(this.label4);
             this.registerPanel.Controls.Add(this.textBox3);
-            this.registerPanel.Controls.Add(this.textBox4);
             this.registerPanel.Location = new System.Drawing.Point(391, 56);
             this.registerPanel.Name = "registerPanel";
-            this.registerPanel.Size = new System.Drawing.Size(372, 351);
+            this.registerPanel.Size = new System.Drawing.Size(389, 351);
             this.registerPanel.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = " ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label5
             // 
@@ -165,16 +181,17 @@
             this.textBox5.Size = new System.Drawing.Size(338, 29);
             this.textBox5.TabIndex = 4;
             // 
-            // button4
+            // RegisterButton
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(14, 255);
-            this.button4.Margin = new System.Windows.Forms.Padding(0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 40);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Register";
-            this.button4.UseVisualStyleBackColor = true;
+            this.RegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegisterButton.Location = new System.Drawing.Point(14, 255);
+            this.RegisterButton.Margin = new System.Windows.Forms.Padding(0);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(95, 40);
+            this.RegisterButton.TabIndex = 3;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
             // 
             // label3
             // 
@@ -208,16 +225,17 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox4.Location = new System.Drawing.Point(14, 39);
+            this.textBox4.Location = new System.Drawing.Point(17, 39);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(338, 29);
-            this.textBox4.TabIndex = 0;
+            this.textBox4.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 451);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(415, 451);
             this.Controls.Add(this.registerPanel);
             this.Controls.Add(this.loginPanel);
             this.Controls.Add(this.showRegisterButton);
@@ -238,7 +256,7 @@
         private System.Windows.Forms.Button showLoginButton;
         private System.Windows.Forms.Button showRegisterButton;
         private System.Windows.Forms.Panel loginPanel;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button LoginButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
@@ -246,10 +264,11 @@
         private System.Windows.Forms.Panel registerPanel;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button RegisterButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox4;
     }
 }
--- project/Form1.cs
+++ project/Form1.cs
@@ -28,12 +28,119 @@ namespace project
         {
             loginPanel.Visible = true;
             registerPanel.Visible = false;
+            textBox4.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            label6.Text = "";
         }
 
         private void showRegisterButton_Click(object sender, EventArgs e)
         {
             loginPanel.Visible = false;
             registerPanel.Visible = true;
+        }
+        private bool checkLoginInFile(string login)
+        {
+            string text;
+
+            List<string> lines = new List<string>();
+            string debug_text="";
+            text = System.IO.File.ReadAllText("Database.txt");
+            for(int i = 0; i < text.Length;++i)
+            {
+                if (text[i] != ' ' && text[i] != '\n')
+                {
+                    debug_text += text[i];
+                }
+                if (text[i] == ' ' || i + 1 == text.Length || text[i] == '\n')
+                {
+                    lines.Add(debug_text);
+                    debug_text = "";
+                }
+            }
+            for(int i = 0; i < lines.Count; i += 2)
+            {
+                if (login == lines[i])
+                    return true;
+            }
+            return false;
+        }
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if(textBox4.TextLength >= 4)
+            {
+                if(!checkLoginInFile(textBox4.Text))
+                {
+                    if(textBox3.TextLength >= 4)
+                    {
+                        if(textBox3.Text == textBox5.Text)
+                        {
+                            string text = System.IO.File.ReadAllText("Database.txt");
+                            System.IO.File.WriteAllText("Database.txt", text + "\n" + textBox4.Text + " " + textBox3.Text);
+                            label6.Text = "Sucess";
+                            label6.ForeColor = Color.Green;
+                        }
+                        else 
+                        {
+                            label6.Text = "Password not equal";
+                            label6.ForeColor = Color.Red;
+                        }
+                    }
+                    else 
+                    {
+                        label6.Text = "Password to short (min 4 symbols)";
+                        label6.ForeColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    label6.Text = "Login already exist";
+                    label6.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                label6.Text = "Login to short (min 4 symbols)";
+                label6.ForeColor = Color.Red;
+            }
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string text;
+
+            List<string> lines = new List<string>();
+            string debug_text = "";
+            text = System.IO.File.ReadAllText("Database.txt");
+            for (int i = 0; i < text.Length; ++i)
+            {
+                if (text[i] != ' ' && text[i] != '\n')
+                {
+                    debug_text += text[i];
+                }
+                if (text[i] == ' ' || i + 1 == text.Length || text[i] == '\n')
+                {
+                    lines.Add(debug_text);
+                    debug_text = "";
+                }
+            }
+            for (int i = 0; i < lines.Count; i += 2)
+            {
+                if (textBox1.Text == lines[i] && textBox2.Text == lines[i + 1])
+                {
+                    MessageBox.Show("Sucess");
+                }
+                if(textBox1.Text == lines[i] && textBox2.Text != lines[i + 1])
+                {
+                    MessageBox.Show("Wrong password!");
+                }
+            }
         }
     }
 }
